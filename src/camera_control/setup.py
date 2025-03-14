811b569044b2777f76f78acc8454c8bad60e46ab
StalkerK0t <vasyap8262@gmail.com>
2025-03-14 10:27:35 +0300

    camerea fix

--- src/camera_control/setup.py
+++ src/camera_control/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [ 
-            'camera_control = camera_control.camera_node:main'
+            'camera = camera_control.camera_node:main',
+            'depth_camera = depth_camera_control.camera_node:main'
         ],
     },
 )
